97380d43f1b9df011a98f3d4365e25f40b97a228
wikipeng <pengjunwei@100tal.com>
2017-07-10 15:25:06 +0800

    ## 安装webpack babel

--- doc/00_init.md
+++ doc/00_init.md
@@ -1,3 +1,4 @@
+[toc]
 
 ##1 新建 index.js
 
@@ -19,3 +20,40 @@ npm install --save-dev nodemon
 https://github.com/kentcdodds/cross-env
 
 npm install --save-dev cross-env
+
+## 4 安装 babel
+
+### babel-register
+地址：https://babeljs.io/docs/usage/babel-register/
+
+npm install babel-register --save-dev
+
+### babel-plugin-webpack-loaders
+https://github.com/istarkov/babel-plugin-webpack-loaders
+
+npm install --save-dev babel-cli babel-plugin-webpack-loaders
+npm install --save-dev babel-plugin-webpack-loaders
+
+### babel-polyfill
+https://babeljs.io/docs/usage/polyfill/
+
+npm install --save babel-polyfill
+
+## 5 安装 webpack
+
+https://webpack.github.io/
+
+npm install --save-dev webpack
+
+
+
+## package.json
+### ava
+ava - 未来的test runner
+http://i5ting.github.io/ava-practice/
+http://blog.gezhiqiang.com/2016/11/22/ava-md/
+
+### nyc
+nyc - 代码覆盖率
+http://ourjs.com/detail/5738493888feaf2d031d24fa
+http://blog.gezhiqiang.com/2016/11/22/nyc-md/
---
+++ webpack.config.babel.js
@@ -0,0 +1,48 @@
+var cssModulesIdentName = '[name]__[local]__[hash:base64:5]';
+if (process.env.NODE_ENV === 'production') {
+  cssModulesIdentName = '[hash:base64]';
+}
+
+module.exports = {
+  output: {
+    publicPath: '/',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [
+      'client',
+      'node_modules',
+    ],
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: cssModulesIdentName,
+              importLoaders: 1,
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      },
+    ],
+  },
+};
